992776f173e3d27fd8284e5391acdb97039d7fee
Sunil-Patel-21 <patelsunil60273@gmail.com>
2025-11-09 14:59:10 +0530

    create landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import LandingPage from './pages/Landing'
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <Routes>
+
+        <Route path="/" element={<LandingPage />} />
+
+
+      </Routes>
+    </>
   )
 }
 
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import "../App.css";
 
 function LandingPage() {
-  return <div>Landing Page</div>;
+  return <div className="landingPageContainer">
+    <nav>
+        <div className="navHeader">
+            <h2>Live Video Call</h2>
+        </div>
+        <div className="navList">
+            <p>Join as Gust</p>
+            <p>Register</p>
+            <div role="button">
+                <p>Login</p>
+            </div>
+        </div>
+    </nav>
+
+    <div className="landingMainContainer">
+        <div>
+            <h1>
+                <span style={{color:"#FF9839"}}>Connect </span> 
+                with your loved ones</h1>
+            <p>Cover a distance by Live Video Call</p>
+            <div role="button">
+                <Link to={"/home"}>Get Started</Link>
+            </div>
+        </div>
+        <div>
+            <img src="/mobile.png" alt="mobile" />
+        </div>
+    </div>
+
+  </div>;
 }
 
 export default LandingPage;
